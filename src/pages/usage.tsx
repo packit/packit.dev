6fc14e4ad93f649f8d512f24bf99d149dddcba95
Matej Focko <mfocko@redhat.com>
2023-07-20 13:59:58 +0200

    fix: regenerate usage graphs and adjust page
    Signed-off-by: Matej Focko <mfocko@redhat.com>

--- src/pages/usage.tsx
+++ src/pages/usage.tsx
@@ -1,52 +1,24 @@
 import React from "react";
 import Layout from "@theme/Layout";
 
+import UsageGraph from "../components/UsageGraph";
+
+const USAGES = [
+  "/img/usage/srpm_builds",
+  "/img/usage/copr_build_groups",
+  "/img/usage/koji_build_groups",
+  "/img/usage/vm_image_build_targets",
+  "/img/usage/tft_test_run_groups",
+  "/img/usage/sync_release_runs",
+];
+
 export default function Usage() {
   return (
     <Layout title="Usage" description="Usage statistics of Packit Service">
       <main className="container">
-        <figure className="row">
-          <embed
-            className="col"
-            type="image/svg+xml"
-            src="/img/usage/srpm_builds.svg"
-          />
-        </figure>
-        <figure className="row">
-          <embed
-            className="col"
-            type="image/svg+xml"
-            src="/img/usage/copr_build_groups.svg"
-          />
-        </figure>
-        <figure className="row">
-          <embed
-            className="col"
-            type="image/svg+xml"
-            src="/img/usage/koji_build_groups.svg"
-          />
-        </figure>
-        <figure className="row">
-          <embed
-            className="col"
-            type="image/svg+xml"
-            src="/img/usage/vm_image_build_targets.svg"
-          />
-        </figure>
-        <figure className="row">
-          <embed
-            className="col"
-            type="image/svg+xml"
-            src="/img/usage/tft_test_run_groups.svg"
-          />
-        </figure>
-        <figure className="row">
-          <embed
-            className="col"
-            type="image/svg+xml"
-            src="/img/usage/sync_release_runs.svg"
-          />
-        </figure>
+        {USAGES.map((path) => (
+          <UsageGraph src={path} />
+        ))}
       </main>
     </Layout>
   );
